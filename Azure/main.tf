2a86271ff311455bac56a8849147710f6c817342
Alex Keilin <37543452+slemik1@users.noreply.github.com>
2020-12-22 17:52:05 +0200

    Update main.tf

--- Azure/main.tf
+++ Azure/main.tf
@@ -3,200 +3,144 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "azure_nginx_group" {
-    name     = "myResourceGroup"
-    location = "eastus"
-
-    tags = {
-        environment = "Terraform Demo"
-    }
+ name     = "nginx"
+ location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "azure_nginx_network" {
-    name                = "myVnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = "eastus"
-    resource_group_name = azurerm_resource_group.azure_nginx_group.name
-
-    tags = {
-        environment = "Demo"
-    }
+ name                = "Vnet"
+ address_space       = ["10.0.0.0/16"]
+ location            = azurerm_resource_group.azure_nginx_group.location
+ resource_group_name = azurerm_resource_group.azure_nginx_group.name
 }
 
 resource "azurerm_subnet" "azure_nginx_subnet" {
-    name                 = "mySubnet"
-    resource_group_name  = azurerm_resource_group.azure_nginx_group.name
-    virtual_network_name = azurerm_virtual_network.azure_nginx_network.name
-    address_prefixes       = ["10.0.1.0/24"]
+ name                 = "subnet"
+ resource_group_name  = azurerm_resource_group.azure_nginx_group.name
+ virtual_network_name = azurerm_virtual_network.azure_nginx_network.name
+ address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "azure_nginx_public_ip" {
-    name                         = "myPublicIP"
-    location                     = "eastus"
-    resource_group_name          = azurerm_resource_group.azure_nginx_group.name
-    allocation_method            = "Dynamic"
-
-    tags = {
-        environment = "Demo"
-    }
+ name                         = "PublicIPAddress"
+ location                     = azurerm_resource_group.azure_nginx_group.location
+ resource_group_name          = azurerm_resource_group.azure_nginx_group.name
+ allocation_method            = "Static"
 }
 
-resource "azurerm_network_security_group" "nginx_nsg" {
-    name                = "nginx_security_group"
-    location            = "eastus"
-    resource_group_name = azurerm_resource_group.azure_nginx_group.name
+resource "azurerm_lb" "azure_nginx_lb" {
+ name                = "loadBalancer"
+ location            = azurerm_resource_group.azure_nginx_group.location
+ resource_group_name = azurerm_resource_group.azure_nginx_group.name
 
-    security_rule {
-        name                       = "SSH"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-    security_rule {
-        name                       = "HTTP"
-        priority                   = 100
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "80"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
+ frontend_ip_configuration {
+   name                 = "PublicIPAddress"
+   public_ip_address_id = azurerm_public_ip.azure_nginx_public_ip.id
+ }
+}
 
-    tags = {
-        environment = "Demo"
-    }
+resource "azurerm_lb_backend_address_pool" "azure_nginx_backend_address_pool" {
+ resource_group_name = azurerm_resource_group.azure_nginx_group.name
+ loadbalancer_id     = azurerm_lb.azure_nginx_lb.id
+ name                = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_nat_pool" "example" {
+  resource_group_name            = azurerm_resource_group.azure_nginx_group.name
+  loadbalancer_id                = azurerm_lb.azure_nginx_lb.id
+  name                           = "SampleApplicationPool"
+  protocol                       = "Tcp"
+  frontend_port_start            = 80
+  frontend_port_end              = 81
+  backend_port                   = 8080
+  frontend_ip_configuration_name = "PublicIPAddress"
 }
 
 resource "azurerm_network_interface" "nginx_nic" {
-    name                      = "myNIC"
-    location                  = "eastus"
-    resource_group_name       = azurerm_resource_group.azure_nginx_group.name
+ count               = 2
+ name                = "acctni${count.index}"
+ location            = azurerm_resource_group.azure_nginx_group.location
+ resource_group_name = azurerm_resource_group.azure_nginx_group.name
 
-    ip_configuration {
-        name                          = "myNicConfiguration"
-        subnet_id                     = azurerm_subnet.azure_nginx_subnet.id
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = azurerm_public_ip.azure_nginx_public_ip.id
-    }
-
-    tags = {
-        environment = "Demo"
-    }
+ ip_configuration {
+   name                          = "testConfiguration"
+   subnet_id                     = azurerm_subnet.azure_nginx_subnet.id
+   private_ip_address_allocation = "dynamic"
+ }
 }
 
-resource "azurerm_network_interface_security_group_association" "example" {
-    network_interface_id      = azurerm_network_interface.nginx_nic.id
-    network_security_group_id = azurerm_network_security_group.nginx_nsg.id
+resource "azurerm_managed_disk" "nginx_disk" {
+ count                = 2
+ name                 = "datadisk_existing_${count.index}"
+ location             = azurerm_resource_group.azure_nginx_group.location
+ resource_group_name  = azurerm_resource_group.azure_nginx_group.name
+ storage_account_type = "Standard_LRS"
+ create_option        = "Empty"
+ disk_size_gb         = "1023"
 }
 
-resource "random_id" "randomId" {
-    keepers = {
-        resource_group = azurerm_resource_group.azure_nginx_group.name
-    }
-
-    byte_length = 8
+resource "azurerm_availability_set" "nginx_avset" {
+ name                         = "avset"
+ location                     = azurerm_resource_group.azure_nginx_group.location
+ resource_group_name          = azurerm_resource_group.azure_nginx_group.name
+ platform_fault_domain_count  = 2
+ platform_update_domain_count = 2
+ managed                      = true
 }
 
-resource "azurerm_storage_account" "mystorageaccount" {
-    name                        = "diag${random_id.randomId.hex}"
-    resource_group_name         = azurerm_resource_group.azure_nginx_group.name
-    location                    = "eastus"
-    account_tier                = "Standard"
-    account_replication_type    = "LRS"
+resource "azurerm_virtual_machine" "nginx_vm" {
+ count                 = 2
+ name                  = "acctvm${count.index}"
+ location              = azurerm_resource_group.azure_nginx_group.location
+ availability_set_id   = azurerm_availability_set.nginx_avset.id
+ resource_group_name   = azurerm_resource_group.azure_nginx_group.name
+ network_interface_ids = [element(azurerm_network_interface.nginx_nic.*.id, count.index)]
+ vm_size               = "Standard_DS1_v2"
+ 
 
-    tags = {
-        environment = "Demo"
-    }
+ storage_image_reference {
+   publisher = "Canonical"
+   offer     = "UbuntuServer"
+   sku       = "16.04-LTS"
+   version   = "latest"
+ }
+
+ storage_os_disk {
+   name              = "myosdisk${count.index}"
+   caching           = "ReadWrite"
+   create_option     = "FromImage"
+   managed_disk_type = "Standard_LRS"
+ }
+
+ # Optional data disks
+ storage_data_disk {
+   name              = "datadisk_new_${count.index}"
+   managed_disk_type = "Standard_LRS"
+   create_option     = "Empty"
+   lun               = 0
+   disk_size_gb      = "1023"
+ }
+
+ storage_data_disk {
+   name            = element(azurerm_managed_disk.nginx_disk.*.name, count.index)
+   managed_disk_id = element(azurerm_managed_disk.nginx_disk.*.id, count.index)
+   create_option   = "Attach"
+   lun             = 1
+   disk_size_gb    = element(azurerm_managed_disk.nginx_disk.*.disk_size_gb, count.index)
+ }
+
+ os_profile {
+   computer_name  = "hostname"
+   admin_username = "azureuser"
+   admin_password = "JZg-#9cH+S62Skp+gS86a+!5nV%&C62m"
+   custom_data    = filebase64("./install.sh")
+ }
+
+ os_profile_linux_config {
+   disable_password_authentication = false
+ }
+
+ tags = {
+   environment = "staging"
+ }
 }
-
-resource "tls_private_key" "example_ssh" {
-  algorithm = "RSA"
-  rsa_bits = 4096
-}
-output "tls_private_key" { value = tls_private_key.example_ssh.private_key_pem }
-
-resource "azurerm_linux_virtual_machine" "nginx-1" {
-    name                  = "nginx-1"
-    location              = "eastus"
-    resource_group_name   = azurerm_resource_group.azure_nginx_group.name
-    network_interface_ids = [azurerm_network_interface.nginx_nic.id]
-    size                  = "Standard_DS1_v2"
-    custom_data           = filebase64("./install.sh")
-
-    os_disk {
-        name              = "myOsDisk"
-        caching           = "ReadWrite"
-        storage_account_type = "Premium_LRS"
-    }
-
-    source_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "18.04-LTS"
-        version   = "latest"
-    }
-
-    computer_name  = "Nginx-1"
-    admin_username = "azureuser"
-    disable_password_authentication = true
-
-    admin_ssh_key {
-        username       = "azureuser"
-        public_key     = tls_private_key.example_ssh.public_key_openssh
-    }
-
-    boot_diagnostics {
-        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
-    }
-
-    tags = {
-        environment = "nginx-1"
-    }
-}
-
-/*
-resource "azurerm_linux_virtual_machine" "nginx-2" {
-    name                  = "nginx-2"
-    location              = "eastus"
-    resource_group_name   = azurerm_resource_group.azure_nginx_group.name
-    network_interface_ids = [azurerm_network_interface.nginx_nic.id]
-    size                  = "Standard_DS1_v2"
-    custom_data           = filebase64("./install.sh")
-
-    os_disk {
-        name              = "myOsDisk"
-        caching           = "ReadWrite"
-        storage_account_type = "Premium_LRS"
-    }
-
-    source_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "18.04-LTS"
-        version   = "latest"
-    }
-
-    computer_name  = "Nginx-2"
-    admin_username = "azureuser"
-    disable_password_authentication = true
-
-    admin_ssh_key {
-        username       = "azureuser"
-        public_key     = tls_private_key.example_ssh.public_key_openssh
-    }
-
-    boot_diagnostics {
-        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
-    }
-
-    tags = {
-        environment = "nginx-2"
-    }
-}
-*/
