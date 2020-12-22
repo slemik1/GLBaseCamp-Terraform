c2dd195ccee6edb36428d0222e7222f9bcc270d0
Alex Keilin <37543452+slemik1@users.noreply.github.com>
2020-12-22 17:57:12 +0200

    Update install.sh

--- Azure/install.sh
+++ Azure/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-fet install nginx -y
+sudo apt-get -y update
+sudo apt-fet -y install nginx
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform!" > /var/www/html/index.html
 sudo service nginx start
